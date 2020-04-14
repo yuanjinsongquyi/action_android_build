6e9ec311dd2efe5cc641c04908d5e7ec74d54be8
yuanjinsongquyi <120960084@qq.com>
2020-04-14 18:51:00 +0800

    修改

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 hub checkout master
-VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-APP_NAME=`grep -oP 'appName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-VERSION_CODE=`grep -oP 'versionCode "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/google/debug/spendingtracker_v1.0_debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
-curl -F 'file=@app/build/outputs/apk/google/debug/spendingtracker_v1.0_debug.apk' -F '_api_key=c223e000588468f5a50ad58bf40a20f4' https://www.pgyer.com/apiv2/app/upload
+VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./app/build.gradle`
+VERSION_CODE=`grep -oP ' (?<=versionCode\s)\d+' ./app/build.gradle`
+FILENAME="${APP_NAME} - v${VERSION_NAME}-${VERSION_CODE}"
+if [ ${TYPEPACKAGE}="release" ]; then
+hub release create -a ./app/build/outputs/apk/google/release/"${FILENAME}.apk" -m "${APP_NAME} - v${VERSION_NAME}-${VERSION_CODE}" $(date +%Y%m%d%H%M%S)
+elif [ ${TYPEPACKAGE}="debug" ]; then
+curl -F "file=@app/build/outputs/apk/google/debug/${FILENAME}.apk" -F "_api_key=${CI_TOKEN}" https://www.pgyer.com/apiv2/app/upload
+fi
