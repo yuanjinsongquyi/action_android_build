8aed3b1905e4610e1d4f2140da8561b78f3a7945
yuanjinsongquyi <120960084@qq.com>
2020-04-15 16:15:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,8 @@
 
 hub checkout master
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./app/build.gradle`
-VERSION_CODE=`grep -oP ' (?<=versionCode\s)\d+' ./app/build.gradle`
-FILENAME="${APP_NAME} - v${VERSION_NAME}-${VERSION_CODE}"
-if [ ${TYPEPACKAGE}="release" ]; then
-hub release create -a ./app/build/outputs/apk/google/release/"${FILENAME}.apk" -m "${APP_NAME} - v${VERSION_NAME}-${VERSION_CODE}" $(date +%Y%m%d%H%M%S)
-elif [ ${TYPEPACKAGE}="debug" ]; then
-curl -F "file=@app/build/outputs/apk/google/debug/${FILENAME}.apk" -F "_api_key=${CI_TOKEN}" https://www.pgyer.com/apiv2/app/upload
-fi
+VERSION_CODE=`grep -oP '(?<=versionCode\s)\d+' ./app/build.gradle`
+FILENAME="${APP_NAME}-v${VERSION_NAME}-${VERSION_CODE}"
+TYPEPAC=${TYPEPACKAGE}
+echo ${TYPEPACKAGE}
+hub release create -a ./app/build/outputs/apk/google/release/"${FILENAME}.apk" -m "${APP_NAME}-v${VERSION_NAME}-${VERSION_CODE}" $(date +%Y%m%d%H%M%S)
